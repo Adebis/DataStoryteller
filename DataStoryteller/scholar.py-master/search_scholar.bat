4d1e137a9748aa974cb4b203e513adad2c45532b
Adebis <zevsmiles@gmail.com>
2017-08-27 19:34:12 -0400

    notes

--- DataStoryteller/scholar.py-master/search_scholar.bat
+++
@@ -1,3 +0,0 @@
-FOR /L %%A IN (1937,1,2017) DO py -3 scholar.py -p "machine learning" --after %%A --before %%A -c 100 >> %%A.txt
-
-REM py -3 scholar.py -p "machine learning" --after 1940 --before 1940 -c 100 >> scholar_out_1940.txt
